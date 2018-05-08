3b9516f098f8068aeae646338f49af1c3aefb416
sssmi <simeon.kerkola@gmail.com>
2018-05-08 12:43:04 +0300

    Wire up removePost from the feed

--- src/components/PostFeedItem/PostFeedItem.js
+++ src/components/PostFeedItem/PostFeedItem.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { removePost } from '../../actions/posts';
 
 const PostFeedItem = ({
-  author, title, body, createdAt,
+  dispatch, id, author, title, body, createdAt,
 }) => (
   <div>
     <h2>{title}</h2>
     <h3>{author}</h3>
     <h4>{createdAt}</h4>
     <p>{body}</p>
+    <button
+      onClick={() => {
+        dispatch(removePost({ id }));
+        console.log('click');
+      }}
+    >
+      remove
+    </button>
   </div>
 );
 
-export default PostFeedItem;
+export default connect()(PostFeedItem);

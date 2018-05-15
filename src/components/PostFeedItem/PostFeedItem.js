a74d0064763721c1cc7adac4d130c844e1680237
sssmi <simeon.kerkola@gmail.com>
2018-05-15 22:55:41 +0300

    Setup formatting for createdAt

--- src/components/PostFeedItem/PostFeedItem.js
+++ src/components/PostFeedItem/PostFeedItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 const PostFeedItem = ({
   id, author, title, body, createdAt, totalLikes, totalComments,
@@ -7,7 +8,7 @@ const PostFeedItem = ({
   <div>
     <h2>{title}</h2>
     <h3>{author}</h3>
-    <h4>{createdAt}</h4>
+    <h4>{moment(createdAt).format('ddd MMM Mo YYYY @ H:m ')}</h4>
     <p>{body}</p>
     <div>
       <span>likes: {totalLikes}</span> <span>comments: {totalComments}</span>

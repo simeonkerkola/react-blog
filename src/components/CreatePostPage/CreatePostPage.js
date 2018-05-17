6ca25b104b0de0c01fd56cbc2171d649857ede7d
sssmi <simeon.kerkola@gmail.com>
2018-05-17 20:16:48 +0300

    mapDispatchToProps

--- src/components/CreatePostPage/CreatePostPage.js
+++ src/components/CreatePostPage/CreatePostPage.js
@@ -3,22 +3,21 @@ import { connect } from 'react-redux';
 import PostForm from '../PostForm/PostForm';
 import { startCreatePost } from '../../actions/posts';
 
-const CreatePostPage = (props) => {
-  const createNewPost = (newPost) => {
-    console.log(newPost);
-    props.dispatch(startCreatePost(newPost));
-    props.history.push('/');
-  };
-  return (
-    <div>
-      CreatePostPage
-      {console.log(props)}
-      <PostForm
-        // This prop is called in PostForm onSubmit function
-        onSubmit={createNewPost}
-      />
-    </div>
-  );
-};
+const CreatePostPage = props => (
+  <div>
+    CreatePostPage
+    <PostForm
+      // This prop is called in PostForm onSubmit function
+      onSubmit={props.createNewPost}
+    />
+  </div>
+);
 
-export default connect()(CreatePostPage);
+const mapDispatchToProps = (dispatch, props) => ({
+  createNewPost: (post) => {
+    dispatch(startCreatePost(post));
+    props.history.push('/');
+  },
+});
+
+export default connect(undefined, mapDispatchToProps)(CreatePostPage);

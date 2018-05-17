2feb36f0a079267750b1a62e6e6d5ac327c13b45
sssmi <simeon.kerkola@gmail.com>
2018-05-17 20:48:25 +0300

    Create Post page to class based component

--- src/components/CreatePostPage/CreatePostPage.js
+++ src/components/CreatePostPage/CreatePostPage.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PostForm from '../PostForm/PostForm';
 import { startCreatePost } from '../../actions/posts';
 
-const CreatePostPage = props => (
-  <div>
-    CreatePostPage
-    <PostForm
-      // This prop is called in PostForm onSubmit function
-      onSubmit={props.createNewPost}
-    />
-  </div>
-);
+class CreatePostPage extends Component {
+  onSubmit = (post) => {
+    this.props.onSubmit(post);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        CreatePostPage
+        <PostForm
+          // This prop is called in PostForm onSubmit function
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = (dispatch, props) => ({
-  createNewPost: (post) => {
-    dispatch(startCreatePost(post));
-    props.history.push('/');
-  },
+  onSubmit: post => dispatch(startCreatePost(post)),
 });
 
 export default connect(undefined, mapDispatchToProps)(CreatePostPage);

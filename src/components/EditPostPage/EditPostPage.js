795701492f7b1bd5534bf13c03bd912be137fc60
sssmi <simeon.kerkola@gmail.com>
2018-05-17 23:45:04 +0300

    Edit Post page to class based component

--- src/components/EditPostPage/EditPostPage.js
+++ src/components/EditPostPage/EditPostPage.js
@@ -1,33 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import { connect } from 'react-redux';
 
 import PostForm from '../PostForm/PostForm';
 import { editPost, removePost } from '../../actions/posts';
 
-const EditPostPage = (props) => {
-  const updatePost = (post) => {
-    props.dispatch(editPost(props.post.id, post));
-    props.history.push('/');
+class EditPostPage extends Component {
+  onSubmit = (post) => {
+    this.props.editPost(this.props.post.id, post);
+    this.props.history.push('/');
   };
-  return (
-    <div>
-      <h2>Edit Post</h2>
-      <PostForm post={props.post} onSubmit={updatePost} />
-      <button
-        onClick={() => {
-          props.dispatch(removePost({ id: props.post.id }));
-          props.history.push('/');
-        }}
-      >
-        remove
-      </button>
-    </div>
-  );
-};
+  onRemove = () => {
+    this.props.removePost({ id: this.props.post.id });
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <h2>Edit Post</h2>
+        <PostForm post={this.props.post} onSubmit={this.onSubmit} />
+        <button onClick={this.onRemove}>remove</button>
+      </div>
+    );
+  }
+}
 
 // Id comes from the addressbar
 const mapStateToProps = (state, props) => ({
   post: state.posts.find(post => post.id === props.match.params.id),
 });
 
-export default connect(mapStateToProps)(EditPostPage);
+const mapDispatchToProps = dispatch => ({
+  editPost: (id, updates) => dispatch(editPost(id, updates)),
+  removePost: data => dispatch(removePost(data)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditPostPage);

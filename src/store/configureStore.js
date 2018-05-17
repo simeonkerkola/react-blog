c7a7df72b8bccf493d6505dc24cd56d9c918de0b
sssmi <simeon.kerkola@gmail.com>
2018-05-17 07:26:55 +0300

    Add redux thunk middleware and configure devtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import postsReducer from '../reducers/posts';
 import filtersReducer from '../reducers/filters';
+
+const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
   const store = createStore(
     combineReducers({ posts: postsReducer, filters: filtersReducer }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composeEnchancers(applyMiddleware(thunk)),
   );
   return store;
 };

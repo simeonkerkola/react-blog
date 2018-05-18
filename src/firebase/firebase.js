e8f11a7746856ad94954bd9b7ab571495c5cfe13
sssmi <simeon.kerkola@gmail.com>
2018-05-18 20:04:15 +0300

    Damn u Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/database';
 
+console.log(process.env);
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,

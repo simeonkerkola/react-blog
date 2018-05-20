1121d7df1a9a4922e606f0e08381ff2e719a346a
sssmi <simeon.kerkola@gmail.com>
2018-05-20 22:54:04 +0300

    Header to stateless component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,51 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { startLogin, startLogout } from '../../actions/auth';
 
-class Header extends Component {
-  handleAuthentication = () => {
-    if (this.props.isAuthenticated) {
-      this.props.startLogout();
-    } else {
-      this.props.startLogin();
-    }
-  };
-  render() {
-    return (
-      <header>
-        {/* {console.log(this.props.isAuthenticated)} */}
-        <h1>Blog</h1>
-        <nav>
-          <NavLink to="/" activeClassName="is-active" exact>
-            Home
-          </NavLink>
-          {this.props.isAuthenticated && (
-            <NavLink to="/create" activeClassName="is-active">
-              Create new
-            </NavLink>
-          )}
-          {this.props.isAuthenticated && (
-            <NavLink to={`/user/${this.props.userId}`} activeClassName="is-active">
-              My page
-            </NavLink>
-          )}
-          <NavLink to="/help" activeClassName="is-active">
-            Help
-          </NavLink>
-          <button onClick={this.handleAuthentication}>
-            {this.props.isAuthenticated ? 'Logout' : 'Login'}
-          </button>
-        </nav>
-      </header>
-    );
-  }
-}
+const Header = props => (
+  <header>
+    {console.log(props)}
+    <h1>Blog</h1>
+    <nav>
+      <NavLink to="/" activeClassName="is-active" exact>
+        Home
+      </NavLink>
+      {props.isAuthenticated && (
+        <NavLink to="/create" activeClassName="is-active">
+          Create new
+        </NavLink>
+      )}
+      {props.isAuthenticated && (
+        <NavLink to={`/user/${props.userId}`} activeClassName="is-active">
+          My page
+        </NavLink>
+      )}
+      <NavLink to="/help" activeClassName="is-active">
+        Help
+      </NavLink>
+      {props.isAuthenticated ? (
+        <button onClick={props.startLogout}>Logout</button>
+      ) : (
+        <button onClick={props.startLogin}>Login</button>
+      )}
+    </nav>
+  </header>
+);
 
 const mapStateToProps = state => ({
   isAuthenticated: !!state.auth,
-  userId: state.auth && state.auth.uidd,
+  userId: !!state.auth && state.auth.uid,
 });
 
 const mapDispatchToProps = dispatch => ({

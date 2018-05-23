6299e1e8da704d18fc02e57a1891136fdfb6ec9b
sssmi <simeon.kerkola@gmail.com>
2018-05-23 21:36:33 +0300

    Render tags if any

--- src/components/PostFeedItem/PostFeedItem.js
+++ src/components/PostFeedItem/PostFeedItem.js
@@ -3,18 +3,22 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 
 const PostFeedItem = ({
-  id, author, title, body, createdAt, likes, comments,
-}) => (
-  <div>
-    <h2>{title}</h2>
-    <h3>{author}</h3>
-    <h4>{moment(createdAt).format('ddd MMM Do YYYY @ HH:mm ')}</h4>
-    <p>{body}</p>
+  id, author, title, body, createdAt, likes, comments, tags,
+}) => {
+  const renderTags = () => tags.map(tag => <span key={tag}>{tag} </span>);
+  return (
     <div>
-      <span>likes: {likes}</span> <span>comments: {comments}</span>
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+      <h4>{moment(createdAt).format('ddd MMM Do YYYY @ HH:mm ')}</h4>
+      <p>{body}</p>
+      <div>
+        <span>likes: {likes}</span> <span>comments: {comments}</span>
+      </div>
+      {tags.length > 0 && <div>tags: {renderTags()} </div>}
+      <Link to={`/edit/${id}`}>Edit</Link>
     </div>
-    <Link to={`/edit/${id}`}>Edit</Link>
-  </div>
-);
+  );
+};
 
 export default PostFeedItem;
